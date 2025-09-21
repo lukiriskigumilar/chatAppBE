3d063a1b25141d4e1d3d613af3e2bf84d3ef06d3
Luki Riski <contactlukiriski@gmail.com>
2025-09-21 11:58:05 +0700

    add user schemas users module

---
+++ src/users/schemas/user.schemas.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type userDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, unique: true })
+  id: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const userScheme = SchemaFactory.createForClass(User);
