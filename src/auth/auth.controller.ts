d532fd18882b0e04f4ae41f1987bf6077a2de4bb
Luki Riski <contactlukiriski@gmail.com>
2025-09-22 15:03:56 +0700

    change  naming route to 'api'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
-@Controller('auth')
+@Controller('api')
 export class AuthController {
   constructor(private authService: AuthService) {}
   //register controller
