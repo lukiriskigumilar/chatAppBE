40d7badd584263cbb405f5ed1fe464a78947a984
Luki Riski <contactlukiriski@gmail.com>
2025-09-24 10:39:19 +0700

    fix service; return get and create by dto response profile Feat service : update profile

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -9,6 +9,7 @@ import { Profile } from './schemas/profile.schema';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { getZodiacSign, getHoroscope } from 'src/common/utils/horoscope.util';
 import { ProfileResponse } from './dto/profile-response.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfileService {
@@ -34,7 +35,20 @@ export class ProfileService {
       height: dto.height,
       weight: dto.weight,
     });
-    return profile.save();
+    await profile.save();
+    return {
+      message: 'profile created successfully',
+      data: {
+        displayname: profile.displayName,
+        image: profile.image,
+        birthday: profile.birthday,
+        gender: profile.gender,
+        horoscope: profile.horoscope,
+        zodiac: profile.zodiac,
+        height: profile.height,
+        weight: profile.weight,
+      },
+    };
   }
 
   async getProfileByUserId(userId: string): Promise<ProfileResponse> {
@@ -45,10 +59,48 @@ export class ProfileService {
       message: 'Profile retrieved successfully',
       data: {
         displayname: profile.displayName,
+        image: profile.image,
         birthday: profile.birthday,
         gender: profile.gender,
         horoscope: profile.horoscope,
         zodiac: profile.zodiac,
+        height: profile.height,
+        weight: profile.weight,
+      },
+    };
+  }
+
+  async updateProfile(
+    userId: string,
+    dto: UpdateProfileDto,
+  ): Promise<ProfileResponse> {
+    const profile = await this.profileModel.findOne({ userId }).exec();
+    if (!profile) throw new NotFoundException(' Profile not found ');
+
+    if (dto.displayName) profile.displayName = dto.displayName;
+    if (dto.gender) profile.gender = dto.gender;
+    if (dto.birthday) profile.birthday = dto.birthday;
+
+    const birthday = new Date(dto.birthday);
+    const zodiac = getZodiacSign(birthday);
+    const horoscope = getHoroscope(zodiac);
+    profile.zodiac = zodiac;
+    profile.horoscope = horoscope;
+
+    if (dto.height) profile.height = dto.height;
+    if (dto.weight) profile.weight = dto.weight;
+    await profile.save();
+    return {
+      message: 'update data successfully',
+      data: {
+        displayname: profile.displayName,
+        image: profile.image,
+        birthday: profile.birthday,
+        gender: profile.gender,
+        horoscope: profile.horoscope,
+        zodiac: profile.zodiac,
+        height: profile.height,
+        weight: profile.weight,
       },
     };
   }
