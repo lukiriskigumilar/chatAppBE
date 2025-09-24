f339e58f70f079f74f60fabf43e03b2cdf7a57dc
Luki Riski <contactlukiriski@gmail.com>
2025-09-24 10:33:38 +0700

    add new variable response

--- src/profiles/dto/profile-response.dto.ts
+++ src/profiles/dto/profile-response.dto.ts
@@ -2,9 +2,12 @@ export class ProfileResponse {
   message: string;
   data: {
     displayname: string;
+    image: string;
     gender: string;
     birthday: string;
     zodiac: string;
     horoscope: string;
+    weight: number;
+    height: number;
   };
 }
