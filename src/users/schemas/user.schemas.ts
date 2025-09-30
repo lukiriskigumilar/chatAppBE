469b6477d1454b4765f1bb6d244b2b81043ed6ce
Luki Riski <contactlukiriski@gmail.com>
2025-09-30 22:04:36 +0700

    fix: remove id  field and change by _id default mongoose field

--- src/users/schemas/user.schemas.ts
+++ src/users/schemas/user.schemas.ts
@@ -6,9 +6,6 @@ export type userDocument = User & Document;
 @Schema({ timestamps: true })
 export class User {
   @Prop({ required: true, unique: true })
-  id: string;
-
-  @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true, unique: true })
